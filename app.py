6306c1b26ae3f6933eba615877e6b28c28757b4e
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-09 16:02:33 -0700

    finished part 1-5

--- app.py
+++ app.py
@@ -3,28 +3,42 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-RESPONSES = []
-COUNT = 0
+SURVEY_ANSWERS = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 
 debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def display_survey():
-    return render_template("survey_start.html")
+    """ Load homepage with survey title, instructions, and survey start button """
 
-@app.get(f'/questions/{COUNT}')
-def show_questions():
-    question = survey.questions[COUNT].question
-    choices = survey.questions[COUNT].choices
+    return render_template("survey_start.html",
+    title = survey.title, instructions=survey.instructions)
+
+@app.get('/questions/<count>')
+def show_questions(count):
+    """ Display question with answer choices as radio buttons """
+
+    session["count"] = count
+    question = survey.questions[int(count)].question
+    choices = survey.questions[int(count)].choices
     return render_template("question.html", question = question, choices = choices)
 
 @app.post('/answer')
 def get_answer():
-    response = request.form["answer"]
-    RESPONSES.append(response)
-    COUNT = 1
-    return redirect(f'/questions/{COUNT}')
+    """ Store question answer in SURVEY_ANSWERS and go to next question page
+    If last question was answered, go to completion page"""
+
+    survey_answer = request.form["answer"]
+    SURVEY_ANSWERS.append(survey_answer)
+
+    next_page = int(session["count"])+1
+
+    if next_page == len(survey.questions):
+        return render_template("completion.html")
+
+    return redirect(f'/questions/{next_page}')
+
