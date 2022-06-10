831d3a6c31d043ee5b83fcd9ca372e67d878777e
Eve Morgan <evemorgan27@gmail.com>
2022-06-09 17:22:50 -0700

    finished survey site

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-SURVEY_ANSWERS = []
+# SURVEY_ANSWERS = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -14,34 +14,45 @@ debug = DebugToolbarExtension(app)
 @app.get('/')
 def display_survey():
     """ Load homepage with survey title, instructions, and survey start button """
-
+    session["responses"] = []
     return render_template("survey_start.html", survey=survey)
 
 @app.get('/questions/<int:count>')
 def show_questions(count):
     """ Display question with answer choices as radio buttons """
+    """ If check page number is not equal to the length of responses,
+    redirect to the correct page and return flash message """
+    if count != len(session["responses"]):
+        flash("Trying to access an invalid question!")
+        return redirect(f'/questions/{len(session["responses"])}')
+    if len(survey.questions) == len(session["responses"]):
+        return redirect('/completion')
 
-    # session["count"] = count
     question = survey.questions[count].question
     choices = survey.questions[count].choices
     return render_template("question.html", question = question, choices = choices)
 
 @app.post('/answer')
 def get_answer():
-    """ Store question answer in SURVEY_ANSWERS and go to next question page
+    """ Store question answer in session variable and go to next question page
     If last question was answered, go to completion page"""
 
     survey_answer = request.form["answer"]
-    SURVEY_ANSWERS.append(survey_answer)
 
-    next_page = len(SURVEY_ANSWERS)
-    # next_page = session["count"]+1
+    survey_responses = session["responses"]
+    survey_responses.append(survey_answer)
+    session["responses"] = survey_responses
+
+    next_page = len(session["responses"])
 
     if next_page == len(survey.questions):
         return redirect('/completion')
 
     return redirect(f'/questions/{next_page}')
 
+
 @app.get('/completion')
 def show_completion():
+    """ Display completion page """
     return render_template("completion.html")
+
